c645a0cee9df07eaf0b55a21433fc33cff0279b7
Deanna Minnick <deanna.minnick@fishtownanalytics.com>
2022-09-07 17:57:00 +0000

    add CTEs to staging models

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,6 +1,20 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source('jaffle_shop', 'orders') }}
+with
+
+source as (
+    
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+
+transformed as (
+
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
+    from source
+
+)
+
+select * from transformed
