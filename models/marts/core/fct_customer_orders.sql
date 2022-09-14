2b1a1452c0f0d632112134c2987473b9c14c5333
Deanna Minnick <deanna.minnick@fishtownanalytics.com>
2022-09-14 21:25:05 +0000

    fix payment divide by 100 issue

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -14,7 +14,7 @@ payment_aggregations as (
     select 
         order_id,
         max(created_at) as payment_finalized_date,
-        sum(payment_amount) / 100.0 as total_amount_paid
+        sum(payment_amount) as total_amount_paid
     from payments
     where payment_status <> 'fail'
     group by 1
@@ -45,7 +45,7 @@ customer_orders as (
     
     select
         customer_id,
-        min(order_date) as first_order_date,
+        min(order_date) as fdos,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
     from orders
@@ -66,12 +66,12 @@ running_clv as (
 final as (
     select
         paid_orders.*,
-        case when customer_orders.first_order_date = paid_orders.order_placed_at
+        case when customer_orders.fdos = paid_orders.order_placed_at
             then 'new'
             else 'return'
-            end as new_vs_returning,
+            end as nvsr,
         running_clv.customer_lifetime_value,
-        customer_orders.first_order_date
+        customer_orders.fdos
     from paid_orders
     left join customer_orders
         on paid_orders.customer_id = customer_orders.customer_id
